06839b1df8f1e88379eae1377417bb65bd93d673
Chrigor <chrigor.edu3@gmail.com>
2025-07-08 08:17:40 -0300

    feat: add linter rule for single quote and semicolons as needed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,28 @@
-import { fastify } from 'fastify';
-import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
 
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
+} from 'fastify-type-provider-zod'
 
-import { env } from './env.ts';
+import { env } from './env.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
-});
+})
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', (req, res) => {
-  return res.send('its ok');
-});
+  return res.send('its ok')
+})
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log(`Port: ${env.PORT}`);
-  console.log('[HTTP] Server running');
-});
+  console.log(`Port: ${env.PORT}`)
+  console.log('[HTTP] Server running')
+})
