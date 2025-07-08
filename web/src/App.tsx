945df4d3ffa07b8cecb9e9a00c70b968890798de
Chrigor <chrigor.edu3@gmail.com>
2025-07-08 20:51:45 -0300

    feat: add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
 export function App() {
-  return <div>Hello world</div>
+  return <BrowserRouter>
+    <Routes>
+      <Route index element={<CreateRoom />} />
+      <Route path="/room" element={<Room />} />
+    </Routes>
+  </BrowserRouter>
 }
