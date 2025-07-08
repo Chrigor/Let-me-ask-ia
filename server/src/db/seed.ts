ed08fb4d751b3ba165a96516c920221a69dc6eed
Chrigor <chrigor.edu3@gmail.com>
2025-07-08 09:29:44 -0300

    feat: add seed for rooms table

---
+++ server/src/db/seed.ts
@@ -0,0 +1,20 @@
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+await reset(db, schema)
+
+await seed(db, schema).refine((faker) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: faker.companyName(),
+        description: faker.loremIpsum(),
+      }
+    }
+  }
+})
+
+await sql.end()
+console.log('Database seeded')
