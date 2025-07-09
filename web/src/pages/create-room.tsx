cd8f4f58a54b27b82fd40400d8a99d962c58e33b
Chrigor <chrigor.edu3@gmail.com>
2025-07-09 20:00:31 -0300

    feat: add list rooms and page for room details

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,3 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+
+type GetRoomsAPIReponse = {
+  id: string
+  name: string
+}[]
+
 export function CreateRoom() {
-  return <div>Create room</div>
+  const { data, isLoading } = useQuery<GetRoomsAPIReponse>({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms")
+      return response.json()
+    }
+  })
+
+  return (
+    <div>
+      <div> Create room</div>
+
+      {isLoading && <p>Carregando ... </p>}
+      <div className="flex flex-col gap-1">
+        {data && data.map((data) => (
+          <Link key={data.id} to={`/room/${data.id}`}>
+            {data.name}
+          </Link>
+        ))}
+      </div>
+
+      <Link className="underline" to="/room">
+        Acessar sala
+      </Link>
+    </div>
+  )
 }
