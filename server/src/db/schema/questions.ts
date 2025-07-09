f4e47beaeaf960252cb81588c53c4c70c3e35298
Chrigor <chrigor.edu3@gmail.com>
2025-07-09 20:18:08 -0300

    feat: add questions entity and seed for it

---
+++ server/src/db/schema/questions.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { rooms } from './rooms.ts'
+
+export const questions = pgTable('questions', {
+  id: uuid().primaryKey().defaultRandom(),
+  roomId: uuid().references(() => rooms.id).notNull(),
+  answer: text(),
+  question: text().notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+})
