c13e56dfd3489b77f7b1f75f7561cc601adb652e
Chrigor <chrigor.edu3@gmail.com>
2025-07-10 18:54:36 -0300

    fix: fix where on query

--- server/src/http/routes/get-room-questions.ts
+++ server/src/http/routes/get-room-questions.ts
@@ -3,6 +3,8 @@ import { schema } from '../../db/schema/index.ts'
 import { db } from '../../db/connection.ts'
 import z from 'zod/v4'
 import { desc, eq } from 'drizzle-orm'
+import { id } from 'zod/v4/locales'
+import { rooms } from '../../db/schema/rooms.ts'
 
 export const getRoomsQuestionsRoute: FastifyPluginCallbackZod = (app) => {
   app.get(
@@ -17,10 +19,12 @@ export const getRoomsQuestionsRoute: FastifyPluginCallbackZod = (app) => {
     async (request, reply) => {
       const { roomId } = request.params
 
+      console.log(roomId)
+
       const results = await db
         .select()
         .from(schema.questions)
-        .where(eq(schema.questions.id, roomId))
+        .where(eq(schema.questions.roomId, roomId))
         .orderBy(desc(schema.questions.createdAt))
 
       return reply.send(results)
