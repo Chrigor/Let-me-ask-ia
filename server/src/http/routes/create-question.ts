ee4da87f86df60092a89d153523f36bd2ef1d998
Chrigor <chrigor.edu3@gmail.com>
2025-07-15 17:17:48 -0300

    feat: add get chunks for question on comparator

--- server/src/http/routes/create-question.ts
+++ server/src/http/routes/create-question.ts
@@ -2,6 +2,8 @@ import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { generateEmbbedings } from '../../services/gemini.ts'
+import { and, eq, sql } from 'drizzle-orm'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -20,19 +22,39 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { roomId } = request.params
       const { question } = request.body
 
-      const [insertedQuestion] = await db
-        .insert(schema.questions)
-        .values({
-          roomId,
-          question,
-        })
-        .returning()
+      const embeddings = await generateEmbbedings(question)
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+      const chunks = await db.select({
+        id: schema.audioChunks.id,
+        transcription: schema.audioChunks.transcription,
+        similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`
+      }).from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`
+        )
+        .limit(3)
 
-      if (!insertedQuestion) {
-        throw new Error('Failed to create question')
-      }
+        return reply.status(201).send(chunks)
 
-      return reply.status(201).send(insertedQuestion)
+      // const [insertedQuestion] = await db
+      //   .insert(schema.questions)
+      //   .values({
+      //     roomId,
+      //     question,
+      //   })
+      //   .returning()
+
+      // if (!insertedQuestion) {
+      //   throw new Error('Failed to create question')
+      // }
+
+      // return reply.status(201).send(insertedQuestion)
     }
   )
 }
